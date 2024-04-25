c00b63027ba0d69baba019b0cd796e0f6b1cfd58
包子 <chengbaozi6@qq.com>
2024-04-25 23:52:35 +0800

    exhibition

---
+++ Assets/Scripts_Runtime/ClientMain.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClientMain : MonoBehaviour {
+    // Start is called before the first frame update
+    void Awake() {
+        Debug.Log("ClientMain Awake");
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+    }
+}
