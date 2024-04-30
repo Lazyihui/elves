11cc9de80fced8249fb1840c1c583ab48b1b9d35
包子 <chengbaozi6@qq.com>
2024-04-30 23:54:18 +0800

    <feature>add: 增加了地面种类 RulerEntity.cs

--- Assets/Scripts_Runtime/GameBusiness/Entity/BookEntity.cs
+++ Assets/Scripts_Runtime/GameBusiness/Entity/BookEntity.cs
@@ -6,6 +6,15 @@ public class BookEntity : MonoBehaviour {
     public int typeID;
 
     public int id;
+    // 种类
+    // 是否是橡皮
+    public bool isEraser;
+
+    // 是否是书
+    public bool isBook;
+
+    // 是否是尺子
+    public bool isRuler;
 
 
     public BookEntity() {
@@ -13,7 +22,7 @@ public class BookEntity : MonoBehaviour {
     }
 
     public void Ctor() { }
-    
-    
+
+
 
 }
---
+++ Assets/Scripts_Runtime/GameBusiness/Entity/RulerEntity.cs
@@ -0,0 +1,17 @@
+using System;
+using UnityEngine;
+
+public class RulerEntity : MonoBehaviour {
+
+    public int typeID;
+
+    public int id;
+    // 种类
+    // 是否是橡皮
+
+
+    public RulerEntity() { }
+
+    public void Ctor() { }
+
+}
