f1fbecde138c947842f24b3e2029ec36deff0ce1
包子 <chengbaozi6@qq.com>
2024-05-01 22:44:02 +0800

    <WIP>

--- Assets/Scripts_Runtime/Template_Infrastructure/TemplateInfras.cs
+++ Assets/Scripts_Runtime/Template_Infrastructure/TemplateInfras.cs
@@ -9,19 +9,22 @@ using UnityEngine.ResourceManagement.AsyncOperations;
 
 public class TemplateInfras {
 
+    //    foreach (var so in list) {
+    //                     var tm = so.tm;
+    //     ctx.Role_Add(tm);
+    //                 }
     public static void Load(TemplateContext ctx) {
         {
             AssetLabelReference labelReference = new AssetLabelReference();
             labelReference.labelString = "TM_Book";
             var ptr = Addressables.LoadAssetsAsync<BookTM>(labelReference, null);
-
-            IList<BookTM> list = ptr.WaitForCompletion();
-            for(int i = 0; i < list.Count; i++) {
-                BookTM book = list[i];
-                ctx.books.Add(book.id, book);
+            var list = ptr.WaitForCompletion();
+            foreach (var go in list) {
+                ctx.books.Add(go.id, go);
             }
             ctx.bookPtr = ptr;
         }
+
     }
 
     public static void Unload(TemplateContext ctx) {
